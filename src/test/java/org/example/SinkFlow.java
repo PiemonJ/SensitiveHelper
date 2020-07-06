2807d2f0707e2b44ac1cac70f7b0c1929ea0a7f2
jiaxiaofeng5 <jiaxiaofeng5@jd.com>
2020-07-06 16:10:30 +0800

    排序

--- src/test/java/org/example/SinkFlow.java
+++ src/test/java/org/example/SinkFlow.java
@@ -2,8 +2,11 @@ package org.example;
 
 import io.reactivex.Observable;
 import io.reactivex.functions.Function;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 
 public class SinkFlow<T> {
@@ -18,8 +21,12 @@ public class SinkFlow<T> {
         return sinkDataFlow.map(mapping).toList().blockingGet();
     }
 
+    public <V> List<V> CollectSortedList(Function<T,V> mapping, Comparator<? super V> comparator){
+        return sinkDataFlow.map(mapping).toSortedList(comparator).blockingGet();
+    }
+
     public <K,V> Map<K,V> CollectMap(Function<T,K> keySelector,Function<T,V> valueSelector){
         return sinkDataFlow.toMap(keySelector,valueSelector).blockingGet();
-
     }
+
 }
